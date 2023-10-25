a251efd05782aa58361a857e72c441a3c64584cf
Hebert <hebertLandim007@outlook.com>
2023-10-25 10:26:23 -0300

    Input for sending subtitles

--- most-used-words/src/components/HomePage.vue
+++ most-used-words/src/components/HomePage.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="pills">
-        <PillComponent v-for="item in groupedWords" :word="item.word"
-            :amount="item.amount" :key="item.word"/>
+    <div class="home">
+        <v-form>
+            <v-file-input
+            label="Selecione as legendas"
+            prepend-icon="mdi-message-text"
+            append-icon="mdi-send"
+            outlined
+            multiple
+            v-model="files"
+            @click:append="processSubtitles"
+            >
+            </v-file-input>
+        </v-form>
+        
+        <div class="pills">
+            <PillComponent v-for="item in groupedWords" :word="item.word"
+                :amount="item.amount" :key="item.word"/>
+        </div>
     </div>
 </template>
 
@@ -13,17 +28,26 @@ export default{
     },
     data: function (){
         return {
+            files:[],
             groupedWords:[
                 {word:"i", amount:547},
                 {word:"you", amount:478},
                 {word:"it", amount:10}
             ]
         }
+    },
+    methods:{
+        processSubtitles(){
+            console.log(this.files)
+        }
     }
 }
 </script>
 
 <style scoped>
+.home{
+    margin:20px;
+}
 .pills{
     display: flex;
 }
