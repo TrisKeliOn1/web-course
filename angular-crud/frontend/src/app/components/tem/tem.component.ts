38475f47f76f3e781e3b4db3b72df368907066fd
Hebert <hebertLandim007@outlook.com>
2023-10-30 09:46:40 -0300

    Navigation Component

---
+++ angular-crud/frontend/src/app/components/tem/tem.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tem',
+  templateUrl: './tem.component.html',
+  styleUrls: ['./tem.component.css']
+})
+export class TemComponent {
+
+}
