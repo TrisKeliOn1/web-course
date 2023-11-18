cc9c9050a6319989bee2a4aba03c330c4e18619e
Hebert <hebertLandim007@outlook.com>
2023-11-18 14:10:29 -0300

    Create Product Method

--- angular-crud/frontend/src/app/app.module.ts
+++ angular-crud/frontend/src/app/app.module.ts
@@ -19,7 +19,11 @@ import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from  '@angular/material/button';
-import { MatSnackBar, MatSnackBarModule } from  '@angular/material/snack-bar';
+import { MatSnackBarModule } from  '@angular/material/snack-bar';
+
+import { HttpClientModule} from '@angular/common/http'
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +45,8 @@ import { MatSnackBar, MatSnackBarModule } from  '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
